5616c6373eae58e344d3a2258ea3b485b2283c8c
Rayan Ghahari <ryanghahari@gmail.com>
2024-07-12 16:09:14 +0200

    test 3

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
             
             if (window.location.pathname !== "/les_justes.html" && window.location.pathname !== "/les_vies_sauvees.html") {
                 navLinks.forEach(function(link) {
-                    link.classList.add("bleu");
+                    link.classList.add("top");
                 });
             }
         }
